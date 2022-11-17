4b7eb4c5aac44bfdb1841da612045d3432121cc8
denis_polyarush <55382692+DenisDolmatov2020@users.noreply.github.com>
2022-11-17 13:14:19 +0700

    Init project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <div
+        v-for="(event, index) in events"
+        :key="index" class="mt-6 pa-3"
+        :style="{ width: event.width }"
+      >
+        <h3 class="mb-2">{{ event.title }}</h3>
+        <TagsList :tags="tags" :space-between="event.spaceBetween" />
+        <hr class="mx-1" />
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TagsList from '@/components/TagsList'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    TagsList
+  },
+
+  data: () => ({
+    events: [
+      { title: 'Город сыра и мира', width: '100%', spaceBetween: true },
+      { title: 'Экскурсия в национальный парк "Куршская коса"', width: '100%', spaceBetween: false },
+      { title: 'О кирках, рыцарях и замках', width: '50%', spaceBetween: true },
+      { title: 'Экскурсия в Янтарный', width: '50%', spaceBetween: false }
+    ],
+    tags: [
+      { icon: 'mdi-star-outline', text: '5' },
+      { text: 'Обзорная' },
+      { icon: 'mdi-bus', text: 'На автобусе' },
+      { text: '25 марта' },
+      { icon: 'mdi-clock-outline', text: '6 часов' },
+      { icon: 'mdi-plus', text: 'Дополнительный' }
+    ]
+  })
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TagsList.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="block">
+    <div
+      v-for="(tag, index) in tags"
+      :key="index"
+      class="block__item"
+      :class="{ 'block__item-grow': spaceBetween }"
+    >
+      <span class="block__item-dot" >
+         <v-icon color="black" v-text="'mdi-circle-small'" />
+      </span>
+
+      <span class="block__item-tag">
+        <v-icon v-text="tag.icon" />
+        {{ tag.text }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TagsList',
+
+  props: {
+    tags: {
+      type: Array,
+      default: () => []
+    },
+
+    spaceBetween: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.block {
+  width: 100%;
+  height: 30px;
+  overflow: hidden;
+  display: flex;
+  flex-wrap: wrap;
+
+  &__item {
+    display: flex;
+
+    &-grow {
+      flex-grow: 1;
+    }
+
+    &:first-child {
+      flex-grow: 0;
+
+      .block__item-dot {
+        display: none;
+      }
+    }
+
+    &-dot {
+      flex-grow: 1;
+      text-align: center;
+    }
+
+    span {
+      color: #333;
+      text-wrap: none;
+    }
+  }
+}
+</style>
