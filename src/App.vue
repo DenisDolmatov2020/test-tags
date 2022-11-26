20ebbe02e039eb53a083a58cc51da4a89909b96a
denis_polyarush <55382692+DenisDolmatov2020@users.noreply.github.com>
2022-11-26 23:24:56 +0700

    Update logic on JS

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
         :style="{ width: event.width }"
       >
         <h3 class="mb-2">{{ event.title }}</h3>
-        <TagsList :tags="tags" :space-between="event.spaceBetween" />
+        <TagsList :tags="tags" :justify="event.justify" :width="event.width" />
         <hr class="mx-1" />
       </div>
     </v-main>
@@ -26,11 +26,12 @@ export default {
 
   data: () => ({
     events: [
-      { title: 'Город сыра и мира', width: '100%', spaceBetween: true },
-      { title: 'Экскурсия в национальный парк "Куршская коса"', width: '100%', spaceBetween: false },
-      { title: 'О кирках, рыцарях и замках', width: '50%', spaceBetween: true },
-      { title: 'Экскурсия в Янтарный', width: '50%', spaceBetween: false }
+      { title: 'Город сыра и мира', width: '100%', justify: 'space-between' },
+      { title: 'Экскурсия в национальный парк "Куршская коса"', width: '100%', justify: 'left' },
+      { title: 'О кирках, рыцарях и замках', width: '50%', justify: 'space-between' },
+      { title: 'Экскурсия в Янтарный', width: '50%', justify: 'left' }
     ],
+
     tags: [
       { icon: 'mdi-star-outline', text: '5' },
       { text: 'Обзорная' },
--- src/components/TagsList.vue
+++ src/components/TagsList.vue
@@ -1,40 +1,94 @@
-<template functional>
-  <div class="block">
-    <div
-      v-for="(tag, index) in props.tags"
-      :key="index"
-      class="block__item"
-      :class="{ 'block__item-grow': props.spaceBetween }"
-    >
-      <span class="block__item-dot" >
-         <v-icon color="black" v-text="'mdi-circle-small'" />
-      </span>
-
-      <span class="block__item-tag">
+<template>
+  <div ref="block" class="block" :style="{ justifyContent: justify }">
+    <template v-for="(tag, index) in tags">
+      <div ref="tag" :key="'tag_' + index" class="block__item-tag">
         <v-icon v-text="tag.icon" />
         {{ tag.text }}
+      </div>
+      <span ref="dot" :key="index" class="block__item-dot">
+        <v-icon color="black" v-text="'mdi-circle-small'" />
       </span>
-    </div>
+    </template>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App',
+
+  props: {
+    justify: {
+      type: String,
+      default: ''
+    },
+
+    width: {
+      type: String
+    },
+
+    tags: {
+      type: Array
+    }
+  },
+
+  mounted () {
+    window.addEventListener('resize', this.resizeHandler)
+    this.resizeHandler()
+  },
+
+  beforeDestroy () {
+    window.removeEventListener('resize', this.resizeHandler)
+  },
+
+  methods: {
+    resizeHandler () {
+      const blockWidth = this.$refs.block.clientWidth
+      const tags = this.$refs.tag
+      const dots = this.$refs.dot
+
+      let tagsWidth = 0
+      let tagsVisible = 0
+      let enough = true
+
+      for (let i = 0; i < tags.length; i++) {
+        if (enough && (tagsWidth + tags[i].scrollWidth + dots[i].scrollWidth) < blockWidth) {
+          tagsVisible = i
+          tagsWidth += tags[i].scrollWidth + dots[i].scrollWidth
+          tags[i].classList.remove('hidden')
+          dots[i].classList.remove('hidden')
+        } else {
+          enough = false
+          tags[i].classList.add('hidden')
+          dots[i].classList.add('hidden')
+        }
+      }
+
+      dots[tagsVisible].classList.add('hidden')
+    }
+  }
+}
+</script>
+
 <style lang="scss" scoped>
+.hidden {
+  visibility: hidden;
+}
 .block {
   width: 100%;
-  height: 30px;
+  height: 100%;
   overflow: hidden;
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
 
   &__item {
-    display: flex;
+    // display: flex;
 
-    &-grow {
-      flex-grow: 1;
+    &-tag {
+      white-space: nowrap;
     }
 
     &:first-child {
-      flex-grow: 0;
+      // flex-grow: 0;
 
       .block__item-dot {
         display: none;
@@ -42,7 +96,6 @@
     }
 
     &-dot {
-      flex-grow: 1;
       text-align: center;
     }
 
